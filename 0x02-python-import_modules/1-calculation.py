a2f831e5bcc174773307e99da57e823fa1f99f72
“[Sharon <sharonkarwitha@icloud.com>
2023-06-08 17:51:28 +0300

    modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-a = 10
-b = 5
 
-import calculator_1
-
-sum_result = calculator_1.add(a, b)
-sub_result = calculator_1.subtract(a, b)
-mul_result = calculator_1.multiply(a, b)
-div_result = calculator_1.divide(a, b)
-
-print("Sum: {}".format(sum_result))
-print("Subtraction: {}".format(sub_result))
-print("Multiplication: {}".format(mul_result))
-print("Division: {}".format(div_result))
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
