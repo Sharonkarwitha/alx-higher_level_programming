c645bfb976547d5e8c9e44bdbf686b8b2d82a87e
“[Sharon <sharonkarwitha@icloud.com>
2023-06-08 21:50:15 +0300

    import and modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args_vector = len(sys.argv)
-    if (args_vector <= 1):
-        print("{} arguements.".format(args_vector - 1))
-    elif (args_vector == 2):
-        print("{} arguement:".format(args_vector - 1))
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        ("{} arguements:".format(args_vector - 1))
-    for arg_c in range(1, args_vector):
-        print("{}: {}".format(arg_c, sys.argv[arg_c]))
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
